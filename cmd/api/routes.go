64ce15c3f726e10bc9e14d8682fab60a6a4668aa
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-25 22:27:01 +0100

    customizes the httprouter route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,10 @@ func (app *application) routes() *httprouter.Router {
 	// initialize a new httprouter router instance
 	router := httprouter.New()
 
+	// overwritting the httprouter
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// register the relevant methods
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
