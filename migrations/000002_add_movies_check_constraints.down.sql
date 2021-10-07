8c6ef8c6d266bfdc216a76d991f927894ea7200a
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-10-07 12:52:37 +0100

    adds constraints migration for the movies table

---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE
+  movies DROP CONSTRAINT IF EXISTS movies_duration_check;
+ALTER TABLE
+  movies DROP CONSTRAINT IF EXISTS movies_year_check;
+ALTER TABLE
+  movies DROP CONSTRAINT IF EXISTS genres_length_check;
---
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -0,0 +1,18 @@
+ALTER Table
+  movies
+ADD
+  CONSTRAINT movies_duration_check CHECK (runtime >= 0);
+ALTER Table
+  movies
+ADD
+  CONSTRAINT movies_year_check CHECK (
+    year BETWEEN 1888
+    AND date_part('year', now())
+  );
+ALTER Table
+  movies
+ADD
+  CONSTRAINT genres_length_check CHECK (
+    array_length(genres, 1) BETWEEN 1
+    and 5
+  );
