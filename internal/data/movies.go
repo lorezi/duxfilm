9d4b5aa67087953228a9aa966aa4e1cf02b4e3e0
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-10-19 11:30:46 +0100

    change the update returning value from id to version

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -105,7 +105,7 @@ func (m MovieModel) Update(movie *Movie) error {
 		UPDATE movies
 		SET title = $1, year = $2, duration = $3, genres= $4, version = version + 1
 		WHERE id = $5
-		RETURNING id`
+		RETURNING version`
 
 	args := []interface{}{
 		movie.Title,
