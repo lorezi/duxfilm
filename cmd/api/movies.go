2b15eec1a92e591561258c9f6f0fbab6a1e173bb
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-10-21 15:28:38 +0100

    fetch all movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -193,6 +193,15 @@ func (app *application) getMoviesHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 
 }
