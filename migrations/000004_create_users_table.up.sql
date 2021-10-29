40910eabd9eee0f5e662fd9ff92a4f707d52ca0f
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-10-29 17:01:56 +0100

    adds create extension query to fix citext error message

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -1,9 +1,10 @@
 CREATE Table If NOT EXISTS users (
   id bigserial PRIMARY KEY,
   created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW(),
-  name text NOT NULL,
+  username text NOT NULL,
   email citext UNIQUE NOT NULL,
   password_hash bytea NOT NULL,
   activated bool NOT NULL,
   version integer NOT NULL DEFAULT 1
 );
+CREATE EXTENSION citext
