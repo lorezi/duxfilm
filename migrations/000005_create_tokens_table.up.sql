7fe1b91cde2ecfc43e304bb1b31fe957beda6846
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-11-06 17:07:00 +0100

    adds migrations for token's table

---
+++ migrations/000005_create_tokens_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS tokens (
+  hash bytea PRIMARY KEY,
+  user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE,
+  expiry TIMESTAMP(0) WITH time zone NOT NULL,
+  scope TEXT NOT NULL
+)
