381a3721db457c33514b3909ebf96515baaa4293
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-11-15 15:02:22 +0100

    add authentication feature

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -13,16 +13,25 @@ import (
 
 // Define constants for the token scope. For now we just define the scope "activation"
 const (
-	ScopeActivation = "activation"
+	ScopeActivation     = "activation"
+	ScopeAuthentication = "authentication"
 )
 
 // Define a Token struct to hold the data for an individual token. This includes the plaintext and hashed versions of the token, associated user ID, expiry time and scope.
+// Add struct tags to control how the struct appears when encoded to JSON.
 type Token struct {
 	Plaintext string
 	Hash      []byte
 	UserID    int64
 	Expiry    time.Time
 	Scope     string
+}
+
+// Add struct tags to control how the struct appears when encoded to JSON.
+
+type TokenResponse struct {
+	Plaintext string    `json:"token"`
+	Expiry    time.Time `json:"expiry"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
