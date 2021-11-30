9b95572b5fb96fd5720827cb58ca996bbe9e2b54
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-11-30 22:48:26 +0100

    add middleware to intercepts and respond to any preflight requests

--- README.md
+++ README.md
@@ -55,3 +55,4 @@ a JSON API for retrieving and managing information about movies.
 18. CORS
 
 - Supporting multiple dynamic origins
+- Middleware to intercepts and responds to any preflight requests.
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -227,6 +227,8 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		// Add the "Vary: Origin" header.
 		w.Header().Add("Vary", "Origin")
 
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		// Get the value of the request's Origin header.
 		origin := r.Header.Get("Origin")
 
@@ -238,6 +240,18 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 					// If there is a match, then set a "Access-Control-Allow-Origin"
 					// response header with the request origin as the value.
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// Check if the request has the HTTP method OPTIONS and contains the "Access-Control-Request-Method" header.
+					// If it does, then we treat it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers, as discussed previously.
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						// Write the headers along with a 200 OK status and return from the middleware with no further action.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 				}
 			}
 		}
